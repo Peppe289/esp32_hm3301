10aac0592443c0e89a590299c47f0962601e6996
Peppe289 <gsperanza204@protonmail.com>
2025-12-23 15:00:23 +0100

    uart_gps: clean up
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- main/uart_gps.c
+++ main/uart_gps.c
@@ -1,24 +1,67 @@
 #include "driver/gpio.h"
 #include "driver/uart.h"
+#include "esp_compiler.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
-#include "nmea.h"
-#include "gpgll.h"
 #include "gpgga.h"
-#include "gprmc.h"
+#include "gpgll.h"
 #include "gpgsa.h"
-#include "gpvtg.h"
-#include "gptxt.h"
 #include "gpgsv.h"
+#include "gprmc.h"
+#include "gptxt.h"
+#include "gpvtg.h"
+#include "nmea.h"
+
+#include "uart_gps.h"
+
+#define _LOG "UART: "
+
+#define _UART_DEBUG
+
+#ifdef _UART_DEBUG
+#define LOGE(...) fprintf(stderr, _LOG __VA_ARGS__);
+#define LOGI(...) fprintf(stdout, _LOG __VA_ARGS__);
+#define LOGD(...) fprintf(stdout, _LOG __VA_ARGS__);
+#else
+#define LOGE(...)
+#define LOGI(...)
+#define LOGD(...)
+#endif
 
 #define GPS_TXD (GPIO_NUM_21)
 #define GPS_RXD (GPIO_NUM_20)
 #define GPS_UART_PORT UART_NUM_1
 #define GPS_BAUD_RATE 9600
+
+// this is worth to make it more generic later
+// and use macro like this: [nmea_##data_type]
+typedef enum {
+  nmea_unknown = NMEA_UNKNOWN,
+  nmea_gpgga = NMEA_GPGGA,
+  nmea_gpgll = NMEA_GPGLL,
+  nmea_gpgsa = NMEA_GPGSA,
+  nmea_gpgsv = NMEA_GPGSV,
+  nmea_gprmc = NMEA_GPRMC,
+  nmea_gptxt = NMEA_GPTXT,
+  nmea_gpvtg = NMEA_GPVTG
+} nmea_low_t;
+
+//#define NMEA_CAST_DATA(data_type, data) ((nmea_##data_type##_s *)data)
+
+#define NMEA_UART_DATA_SIZE (NMEA_GPVTG + 1)
+
+static void *nmea_uart_data[NMEA_UART_DATA_SIZE] = {
+    [nmea_unknown] = NULL, [nmea_gpgga] = NULL, [nmea_gpgll] = NULL,
+    [nmea_gpgsa] = NULL,   [nmea_gpgsv] = NULL, [nmea_gprmc] = NULL,
+    [nmea_gptxt] = NULL,   [nmea_gpvtg] = NULL,
+};
 
 const uart_config_t uart_config = {
     .baud_rate = GPS_BAUD_RATE,
@@ -36,173 +79,78 @@ void init_gps_uart(void) {
                UART_PIN_NO_CHANGE);
 }
 
-void gps_parse(const char *str)
-{
-	char *start = str;
-	uint8_t run = true;
-	nmea_s *data;
+static void gps_data(nmea_s *data) {
+  // char buf[255];
 
-	// Sync
-	while (*start != '$' && *start != '\0')
-	{
-		start++;
-	}
-	if (*start == '\0')
-		return; // Message not found
+  if (NULL != data) {
+    if (0 < data->errors) {
+      //LOGI("WARN: The sentence struct contains parse errors!\n");
+      return;
+    }
 
-	while (run)
-	{
-		char buf[255];
-		char cpy[255] = {0};
-		int index = 0;
+    if (nmea_uart_data[data->type])
+      nmea_free(nmea_uart_data[data->type]);
 
-		while (true)
-		{
-			if (*start == '\n' || *start == '\0')
-			{
-				start++;
-				if (*start == '\0')
-					run = false;
-				break;
-			}
-			if (*start != '\r')
-			{
-				cpy[index++] = *start;
-			}
-			start++;
-		}
-
-		cpy[index++] = '\r';
-		cpy[index] = '\n';
-
-		// Invalid string
-		if (cpy[0] != '$')
-			continue;
-
-		data = nmea_parse(cpy, strlen(cpy), 0);
-		if (NULL != data)
-		{
-			if (0 < data->errors)
-			{
-				printf("WARN: The sentence struct contains parse errors!\n");
-			}
-
-			if (NMEA_GPGGA == data->type)
-			{
-				printf("GPGGA sentence\n");
-				nmea_gpgga_s *gpgga = (nmea_gpgga_s *)data;
-				printf("Number of satellites: %d\n", gpgga->n_satellites);
-				printf("Altitude: %f %c\n", gpgga->altitude, gpgga->altitude_unit);
-			}
-
-			if (NMEA_GPGLL == data->type)
-			{
-				printf("GPGLL sentence\n");
-				nmea_gpgll_s *pos = (nmea_gpgll_s *)data;
-				printf("Longitude:\n");
-				printf("  Degrees: %d\n", pos->longitude.degrees);
-				printf("  Minutes: %f\n", pos->longitude.minutes);
-				printf("  Cardinal: %c\n", (char)pos->longitude.cardinal);
-				printf("Latitude:\n");
-				printf("  Degrees: %d\n", pos->latitude.degrees);
-				printf("  Minutes: %f\n", pos->latitude.minutes);
-				printf("  Cardinal: %c\n", (char)pos->latitude.cardinal);
-				if (strftime(buf, sizeof(buf), "%H:%M:%S", &pos->time))
-					printf("Time: %s\n", buf);
-			}
-
-			if (NMEA_GPRMC == data->type)
-			{
-				printf("GPRMC sentence\n");
-				nmea_gprmc_s *pos = (nmea_gprmc_s *)data;
-				printf("Longitude:\n");
-				printf("  Degrees: %d\n", pos->longitude.degrees);
-				printf("  Minutes: %f\n", pos->longitude.minutes);
-				printf("  Cardinal: %c\n", (char)pos->longitude.cardinal);
-				printf("Latitude:\n");
-				printf("  Degrees: %d\n", pos->latitude.degrees);
-				printf("  Minutes: %f\n", pos->latitude.minutes);
-				printf("  Cardinal: %c\n", (char)pos->latitude.cardinal);
-				if (strftime(buf, sizeof(buf), "%d %b %T %Y", &pos->date_time))
-					printf("Date & Time: %s\n", buf);
-				printf("Speed, in Knots: %f:\n", pos->gndspd_knots);
-				printf("Track, in degrees: %f\n", pos->track_deg);
-				printf("Magnetic Variation:\n");
-				printf("  Degrees: %f\n", pos->magvar_deg);
-				printf("  Cardinal: %c\n", (char)pos->magvar_cardinal);
-				double adjusted_course = pos->track_deg;
-				if (NMEA_CARDINAL_DIR_EAST == pos->magvar_cardinal)
-				{
-					adjusted_course -= pos->magvar_deg;
-				}
-				else if (NMEA_CARDINAL_DIR_WEST == pos->magvar_cardinal)
-				{
-					adjusted_course += pos->magvar_deg;
-				}
-				else
-				{
-					printf("Invalid Magnetic Variation Direction!!\n");
-				}
-
-				printf("Adjusted Track (heading): %f\n", adjusted_course);
-			}
-
-			if (NMEA_GPGSA == data->type)
-			{
-				nmea_gpgsa_s *gpgsa = (nmea_gpgsa_s *)data;
-
-				printf("GPGSA Sentence:\n");
-				printf("  Mode: %c\n", gpgsa->mode);
-				printf("  Fix:  %d\n", gpgsa->fixtype);
-				printf("  PDOP: %.2lf\n", gpgsa->pdop);
-				printf("  HDOP: %.2lf\n", gpgsa->hdop);
-				printf("  VDOP: %.2lf\n", gpgsa->vdop);
-			}
-
-			if (NMEA_GPGSV == data->type)
-			{
-				nmea_gpgsv_s *gpgsv = (nmea_gpgsv_s *)data;
-
-				printf("GPGSV Sentence:\n");
-				printf("  Num: %d\n", gpgsv->sentences);
-				printf("  ID:  %d\n", gpgsv->sentence_number);
-				printf("  SV:  %d\n", gpgsv->satellites);
-				printf("  #1:  %d %d %d %d\n", gpgsv->sat[0].prn, gpgsv->sat[0].elevation, gpgsv->sat[0].azimuth, gpgsv->sat[0].snr);
-				printf("  #2:  %d %d %d %d\n", gpgsv->sat[1].prn, gpgsv->sat[1].elevation, gpgsv->sat[1].azimuth, gpgsv->sat[1].snr);
-				printf("  #3:  %d %d %d %d\n", gpgsv->sat[2].prn, gpgsv->sat[2].elevation, gpgsv->sat[2].azimuth, gpgsv->sat[2].snr);
-				printf("  #4:  %d %d %d %d\n", gpgsv->sat[3].prn, gpgsv->sat[3].elevation, gpgsv->sat[3].azimuth, gpgsv->sat[3].snr);
-			}
-
-			if (NMEA_GPTXT == data->type)
-			{
-				nmea_gptxt_s *gptxt = (nmea_gptxt_s *)data;
-
-				printf("GPTXT Sentence:\n");
-				printf("  ID: %d %d %d\n", gptxt->id_00, gptxt->id_01, gptxt->id_02);
-				printf("  %s\n", gptxt->text);
-			}
-
-			if (NMEA_GPVTG == data->type)
-			{
-				nmea_gpvtg_s *gpvtg = (nmea_gpvtg_s *)data;
-
-				printf("GPVTG Sentence:\n");
-				printf("  Track [deg]:   %.2lf\n", gpvtg->track_deg);
-				printf("  Speed [kmph]:  %.2lf\n", gpvtg->gndspd_kmph);
-				printf("  Speed [knots]: %.2lf\n", gpvtg->gndspd_knots);
-			}
-
-			nmea_free(data);
-		}
-	}
+    nmea_uart_data[data->type] = data;
+  }
 }
 
+#define endline(x) unlikely((x) == '\0' || (x) == '\n')
+
+static void gps_parse(const char *str) {
+  char *start = (char *)str;
+  static char cpy[100] = {0};
+  static uint8_t index = 0;
+
+  // Sync
+  while (*start != '$' && *start != '\0')
+    start++;
+
+  if (endline(*start))
+    return; // Empty message
+
+  while (true) {
+    index = 0;
+    memset(cpy, 0, sizeof(cpy));
+
+    while (true) {
+      if (endline(*start)) {
+        start++;
+        cpy[index] = '\n';
+
+        if (endline(*start))
+          goto last_row;
+        break;
+      }
+      cpy[index++] = *start;
+      start++;
+    }
+
+    // If data lenght is too long or not start with '$', skip to next line
+    if (unlikely(index >= sizeof(cpy) - 1 || cpy[0] != '$'))
+      continue;
+
+    gps_data(nmea_parse(cpy, strlen(cpy), 0));
+  }
+
+last_row:
+  gps_data(nmea_parse(cpy, strlen(cpy), 0));
+}
+
+#define LENGHT_BUFFER 350
+
 void gps_read_task() {
-  uint8_t *data = (uint8_t *)malloc(256);
-  int len = uart_read_bytes(GPS_UART_PORT, data, 255, 20 / portTICK_PERIOD_MS);
+  static uint8_t *data = NULL;
+
+  if (data == NULL) {
+    data = (uint8_t *)malloc(LENGHT_BUFFER);
+  }
+
+  int len = uart_read_bytes(GPS_UART_PORT, data, LENGHT_BUFFER - 1,
+                            20 / portTICK_PERIOD_MS);
   if (len > 0) {
+    LOGI("\n\n\n%s\n\n\n\n", (char *)data);
     data[len] = '\0';
     gps_parse((char *)data);
   }
-  free(data);
 }
--- main/uart_gps.h
+++ main/uart_gps.h
@@ -1,6 +1,18 @@
 #ifndef __UART_GPS_H__
 #define __UART_GPS_H__
 
+
+#include "driver/uart.h"
+
+#include "gpgga.h"
+#include "gpgll.h"
+#include "gpgsa.h"
+#include "gpgsv.h"
+#include "gprmc.h"
+#include "gptxt.h"
+#include "gpvtg.h"
+#include "nmea.h"
+
 void init_gps_uart(void);
 void gps_read_task();
 
